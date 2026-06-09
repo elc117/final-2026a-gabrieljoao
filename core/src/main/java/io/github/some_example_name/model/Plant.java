15950e87bef07c5c03e060725fa45b916f798d4e
Oropoc <jodanlodig@gmail.com>
2026-06-08 22:39:24 -0300

    cabeçalho objetos e inicio da organização lógica

--- core/src/main/java/io/github/some_example_name/model/Plant.java
+++ core/src/main/java/io/github/some_example_name/model/Plant.java
@@ -1,9 +1,16 @@
 package io.github.some_example_name.model;
 
+import java.util.ArrayList;
 import io.github.some_example_name.enums.PlantGrowth;
 
 public class Plant {
     private int id;
     private String name;
     private PlantGrowth GrowthTime;
+    private int unlockLevel;
+
+    private ArrayList<Recipe> recipes;
+    private ArrayList<Tool> tools;
+
+
 }
---
+++ core/src/main/java/io/github/some_example_name/registry/PlantRegistry.java
@@ -0,0 +1,8 @@
+package io.github.some_example_name.registry;
+
+import java.util.ArrayList;
+
+import io.github.some_example_name.enums.PlantGrowth;
+import io.github.some_example_name.model.Plant;
+import io.github.some_example_name.model.Recipe;
+import io.github.some_example_name.model.Tool;
